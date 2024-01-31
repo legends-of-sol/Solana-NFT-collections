cf8f5287835f09bb6bfbc45adf9d6adef14a74a8
harkl <3zy3@protonmail.ch>
2024-01-31 15:05:53 +1300

    other files and node package

---
+++ CONTRIBUTING.md
@@ -0,0 +1,35 @@
+# Contributing to Solana-NFT-collections
+
+We appreciate your interest in contributing to Solana-NFT-collections! Here are some guidelines to help you get started.
+
+## Reporting Bugs
+
+If you encounter any bugs or issues, please open an issue on our GitHub repository. Be sure to include as much information as possible to help us reproduce and fix the problem.
+
+## Suggesting Enhancements
+
+If you have ideas for new features or improvements, we'd love to hear them! Please open an issue and describe your suggestion in detail.
+
+## Pull Requests
+
+If you'd like to contribute code directly, here's the process:
+
+1. Fork the repository.
+2. Create a new branch for your changes.
+3. Make your changes in your branch.
+4. Submit a pull request with your changes.
+5. Wait for your pull request to be reviewed and merged.
+
+## Code Style
+
+We use ESLint to enforce a consistent coding style. You can check your code for style issues by running `npm run lint` (see `package.json` for details).
+
+## License
+
+By contributing to Solana-NFT-collections, you agree that your contributions will be licensed under the same terms as the original project (Apache License 2.0). See `LICENSE` for details.
+
+## Acknowledgements
+
+We maintain a list of all contributors in `devs.md`. When your pull request is merged, we'll add your name to this list.
+
+Thank you for your interest in contributing to Solana-NFT-collections!
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import {Connection, Commitment, PublicKey} from '@solana/web3.js'
-import 'dotenv/config'
-import * as fsPromise from 'fs/promises'
+import { Connection, Commitment, PublicKey } from "@solana/web3.js";
+import "dotenv/config";
+import * as fsPromise from "fs/promises";
 
 // Override this with your own RPC url or put it in an .env file.
-let rpcEndpoint = process.env.RPC_URL as string
-const conn = new Connection(rpcEndpoint, {commitment: 'finalized' as Commitment});
+let rpcEndpoint = process.env.RPC_URL as string;
+const conn = new Connection(rpcEndpoint, {
+  commitment: "finalized" as Commitment,
+});
 
 // Function taken from https://codereview.stackexchange.com/questions/184459/getting-the-date-in-yyyymmdd-format
 function yyyymmdd() {
@@ -12,13 +14,13 @@ function yyyymmdd() {
   var y = x.getFullYear().toString();
   var m = (x.getMonth() + 1).toString();
   var d = x.getDate().toString();
-  (d.length == 1) && (d = '0' + d);
-  (m.length == 1) && (m = '0' + m);
+  d.length == 1 && (d = "0" + d);
+  m.length == 1 && (m = "0" + m);
   var yyyymmdd = y + m + d;
   return yyyymmdd;
 }
 
-const collectionName = ""
+const collectionName = "";
 
 /*
 This method is old school before DAS existed, if you want newer methods some examples
@@ -26,36 +28,40 @@ can be located https://www.helius.dev/blog/nft-holder-snapshots
 */
 const main = async () => {
   // NOTE: This will be your hashlist if used elsewhere.
-  let nfts = require(`../${collectionName}.json`)
-  let currentNumber = 1
-  const date = yyyymmdd()
+  let nfts = require(`../${collectionName}.json`);
+  let currentNumber = 1;
+  const date = yyyymmdd();
   for await (const nft of nfts) {
     // @ts-ignore
-    console.log(nft)
-    console.log(`${currentNumber}/${nfts.length}`)
-    const mintToken = nft
-    currentNumber = currentNumber + 1
+    console.log(nft);
+    console.log(`${currentNumber}/${nfts.length}`);
+    const mintToken = nft;
+    currentNumber = currentNumber + 1;
     try {
-      const largestAccounts = await conn.getTokenLargestAccounts(new PublicKey(mintToken))
-      const largestAccountInfo = await conn.getParsedAccountInfo(largestAccounts.value[0].address)
+      const largestAccounts = await conn.getTokenLargestAccounts(
+        new PublicKey(mintToken)
+      );
+      const largestAccountInfo = await conn.getParsedAccountInfo(
+        largestAccounts.value[0].address
+      );
       // @ts-ignore
       const owner = largestAccountInfo.value.data.parsed.info.owner;
       // @ts-ignore
-      const details = largestAccountInfo.value.data.parsed
-      
-      const json = {
-        "NFTAddress": mintToken as unknown as Text,
-        "ownerAddress": owner as unknown as Text
-      }
+      const details = largestAccountInfo.value.data.parsed;
 
-      await fsPromise.appendFile(`../${collectionName}_${date}.json`, json)
+      const json = {
+        NFTAddress: mintToken as unknown as Text,
+        ownerAddress: owner as unknown as Text,
+      };
+
+      await fsPromise.appendFile(`../${collectionName}_${date}.json`, json);
       // Can add a setTimeout so you're not spamming the RPCs
-      continue
+      continue;
     } catch (e) {
-      console.error(e)
-      continue
+      console.error(e);
+      continue;
     }
   }
-}
+};
 
-main()
+main();
