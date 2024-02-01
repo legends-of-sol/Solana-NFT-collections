9ce87745e0d355b8637298169bdb49804f3194ce
harkl <3zy3@protonmail.ch>
2024-02-01 17:13:57 +1300

    fetch by creator

--- src/app.js
+++ src/app.js
@@ -7,7 +7,11 @@ const { execSync } = require("child_process");
 program
   .command("snapshot <project_name> <collection_address>")
   .description("Take a snapshot of a project")
-  .option("-r, --rpc [RPC]", "RPC endpoint URL", "https://api.mainnet-beta.solana.com")
+  .option(
+    "-r, --rpc [RPC]",
+    "RPC endpoint URL",
+    "https://api.mainnet-beta.solana.com"
+  )
   .action(async (project_name, collection_address, options) => {
     const RPC = options.rpc;
     const getAssetsByGroup = async () => {
@@ -80,9 +84,128 @@ program
       const metaContent = {
         collectionKey: collection_address,
         name: collectionData.content.metadata.name ?? project_name,
-        image: collectionData.content.links.image || resultData.results[0].content.links.image || "",
+        image:
+          collectionData.content.links.image ||
+          resultData.results[0].content.links.image ||
+          "",
         description: collectionData.content.metadata.description ?? "",
-        url: collectionData.content.links.external_url || resultData.results[0].content.links.external_url || "",
+        url:
+          collectionData.content.links.external_url ||
+          resultData.results[0].content.links.external_url ||
+          "",
+        token_standard:
+          resultData.results[0].content.metadata.token_standard ?? "",
+      };
+      fs.writeFileSync(
+        `${metaDirPath}/${project_name}_meta.json`,
+        JSON.stringify(metaContent, null, 2)
+      );
+
+      // Generate CSV content
+      const csvContent = resultData.results
+        .map((item) => `${item.ownership.owner},${item.id}`)
+        .join("\n");
+      fs.writeFileSync(
+        `${dirPath}/${project_name}_${dateStamp}.csv`,
+        `owner,nftmint\n${csvContent}`
+      );
+
+      // Generate JSON content
+      const jsonContent = resultData.results.map((item) => ({
+        NFTAddress: item.id,
+        ownerAddress: item.ownership.owner,
+      }));
+      fs.writeFileSync(
+        `${dirPath}/${project_name}_${dateStamp}.json`,
+        JSON.stringify(jsonContent, null, 2)
+      );
+
+      // Generate Hashlist JSON
+      const hashlistContent = resultData.results.map((item) => item.id);
+      fs.writeFileSync(
+        `${dirPath}/${project_name}_hashlist_${dateStamp}.json`,
+        JSON.stringify(hashlistContent, null, 2)
+      );
+
+      // Generate Unique Owners CSV
+      const uniqueOwners = [
+        ...new Set(resultData.results.map((item) => item.ownership.owner)),
+      ];
+      const uniqueOwnersCSV = uniqueOwners.join("\n");
+      fs.writeFileSync(
+        `${dirPath}/unique_${project_name}_owners_${dateStamp}.csv`,
+        `owner\n${uniqueOwnersCSV}`
+      );
+
+      console.log(`Snapshot successfully taken for -- ${project_name}`);
+      console.timeEnd("getAssetsByGroup");
+    };
+    await getAssetsByGroup();
+  });
+
+program
+  .command("creator_snapshot <project_name> <creator_address>")
+  .description("Take a snapshot of projects by creator")
+  .option(
+    "-r, --rpc [RPC]",
+    "RPC endpoint URL",
+    "https://api.mainnet-beta.solana.com"
+  )
+  .action(async (project_name, creator_address, options) => {
+    const RPC = options.rpc;
+    const getAssetsByGroup = async () => {
+      console.time("getAssetsByGroup"); // Start the timer
+      let page = 1;
+      let assetList = [];
+
+      while (page) {
+        const response = await fetch(RPC, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            jsonrpc: "2.0",
+            id: "my-id",
+            method: "getAssetsByCreator",
+            params: {
+              creatorAddress: creator_address,
+              onlyVerified: true,
+              page,
+              limit: 1000,
+            },
+          }),
+        });
+        const { result } = await response.json();
+
+        assetList.push(...result.items);
+        if (result.total !== 1000) {
+          page = false;
+        } else {
+          page++;
+        }
+      }
+
+      const resultData = {
+        totalResults: assetList.length,
+        results: assetList,
+      };
+      const dateStamp = dayjs().format("YYYYMMDD");
+      const metaDirPath = path.join(__dirname, `../NFTs/${project_name}`);
+      const dirPath = path.join(
+        __dirname,
+        `../NFTs/${project_name}/${dateStamp}`
+      );
+      if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath, { recursive: true });
+      }
+
+      const metaContent = {
+        creatorAddress: creator_address,
+        name: resultData.results[0].content.metadata.name ?? project_name,
+        image: resultData.results[0].content.links.image || "",
+        description: resultData.results[0].content.metadata.description ?? "",
+        url: resultData.results[0].content.links.external_url || "",
         token_standard:
           resultData.results[0].content.metadata.token_standard ?? "",
       };
