dc6428663a574d67545c75d493bbb4dcb38fe6ed
harkl <3zy3@protonmail.ch>
2024-02-05 14:26:51 +1300

    count banditios and pfp

---
+++ programs/smb_banditos/count_unique_banditios.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const path = require('path');
+
+// Paths to the JSON files
+const jsonFilePathGen2 = path.join(__dirname, 'banditos_gen2.json');
+const jsonFilePathGen3 = path.join(__dirname, 'banditos_gen3.json');
+// Output CSV file path
+const csvFilePath = path.join(__dirname, 'unique_banditos_count.csv');
+
+function countNFTs(jsonData, ownerCounts) {
+  // Count NFTs for each owner
+  jsonData.forEach(item => {
+    const ownerId = item.id;
+    const nftCount = item.nfts.length;
+    if (ownerCounts[ownerId]) {
+      ownerCounts[ownerId] += nftCount;
+    } else {
+      ownerCounts[ownerId] = nftCount;
+    }
+  });
+}
+
+function writeCSV(ownerCounts) {
+  // Prepare CSV content
+  let csvContent = 'owner,count\n';
+
+  // Convert ownerCounts to an array, sort by count in descending order, then iterate
+  const sortedOwnerCounts = Object.entries(ownerCounts).sort((a, b) => b[1] - a[1]);
+  for (const [owner, count] of sortedOwnerCounts) {
+    csvContent += `${owner},${count}\n`;
+  }
+
+  // Generate timestamp
+  const date = new Date();
+  const formattedDate = date.toISOString().split('T')[0].replace(/-/g, ''); // YYYYMMDD format
+  const timestampedCsvFilePath = `${csvFilePath.split('.csv')[0]}_${formattedDate}.csv`;
+
+  // Write CSV file with timestamp in the filename
+  fs.writeFile(timestampedCsvFilePath, csvContent, 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing the CSV file:', err);
+      return;
+    }
+    console.log('CSV file has been saved successfully.');
+  });
+}
+
+function readJSONFile(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject('Error reading the JSON file:', err);
+      } else {
+        resolve(JSON.parse(data));
+      }
+    });
+  });
+}
+
+async function main() {
+  try {
+    const ownerCounts = {};
+    const jsonDataGen2 = await readJSONFile(jsonFilePathGen2);
+    const jsonDataGen3 = await readJSONFile(jsonFilePathGen3);
+
+    countNFTs(jsonDataGen2, ownerCounts);
+    countNFTs(jsonDataGen3, ownerCounts);
+
+    writeCSV(ownerCounts);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
